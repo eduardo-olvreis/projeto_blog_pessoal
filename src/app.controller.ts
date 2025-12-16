c8b4e8773c00fcc103b21965448c4516e74dc7c6
eduardo.reis <eduardo.olvreis@gmail.com>
2025-12-16 14:00:49 -0300

    Criação do repositório sozinho para o Blog Pessoal

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import type { Response } from 'express';
+
+@Controller()
+export class AppController {
+  constructor() {}
+
+  @ApiExcludeEndpoint()
+  @Get()
+  redirect(@Res() resposta: Response) {
+    return resposta.redirect("/swagger")
+  }
+
+}
